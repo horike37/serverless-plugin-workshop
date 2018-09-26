14d192538551112d0750bd8c8f98e83df9b94ce0
horike37 <horike37@gmail.com>
2018-09-26 11:38:36 +0900

    minor tweak

---
+++ lessons/2-making-new-command-plugin/sls-plugin/.serverless_plugins/serverless-download.js
@@ -0,0 +1,16 @@
+'use strict';
+
+class ServerlessDownload {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+    };
+
+    this.hooks = {
+    };
+  }
+}
+
+module.exports = ServerlessDownload;
